c2d108c253d945b0b7b5ab220833d73f6087b441
gabogt3 <62921751+gabogt3@users.noreply.github.com>
2023-10-29 00:40:50 -0500

    Update taller_2_1_i2c_scan.py

--- taller_2/taller_2_1_i2c_scan.py
+++ taller_2/taller_2_1_i2c_scan.py
@@ -1,1 +1,20 @@
+#!/usr/bin/env python
 
+import pigpio
+
+pigpio.exceptions = False # handle errors
+
+pi = pigpio.pi()
+
+for bus in range(2):
+    for x in range(0x08, 0x79):
+        try:
+            h = pi.i2c_open(bus, x)
+            if h >= 0:
+                s = pi.i2c_read_byte(h)
+                if s >= 0:
+                    print("dispositivo encontrado con address {} en el bus {}".format(x, bus))
+                pi.i2c_close(h)
+        except:
+            print("Error al probar con address: "+hex(x))
+pi.stop()
